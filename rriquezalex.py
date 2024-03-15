cb8f06efeace190a18dd9ba0beae64a7585435c2
abetha64 <albabethbirai@gmail.com>
2024-03-15 12:27:18 -0600

    update1

--- rriquezalex.py
+++ rriquezalex.py
@@ -11,16 +11,16 @@ carpeta_nombre="Documentos\\"
 
 with open(carpeta_nombre+"archivo_nuevo.txt","r") as archivo:
  texto=archivo.read()
- tokens=nltk.word_tokenize(texto,"spanish")
+ tokens=nltk.word_tokenize(texto,"spanish")#indica el lenguaje
  riqueza_lexica=riqueza_lexica(tokens)
  #print(riqueza_lexica)
 
-#odatu comoestas
-conteo_individual=tokens.count("el")
-print("TOTAL DE VECES EN TEXTO => ",conteo_individual)
-palabras_totales=len(tokens)
-porcentaje=100*conteo_individual/palabras_totales
-print("el porcentaje es:", porcentaje,"%")
+#odatu comoestas //comentario sin lugar alguna
+conteo_individual=tokens.count("los") #palabra a buscar
+print("TOTAL DE VECES EN TEXTO => ",conteo_individual) #imprime el numero de veces que aparece en el texto
+palabras_totales=len(tokens)#palabrasTotales son aquellas que existen en general en el txto, y te arroja cuantas existens
+porcentaje=100*conteo_individual/palabras_totales #saca el porcentaje
+print("el porcentaje es:", porcentaje,"%") #imprime todo
 
 
  
