61994e8d4b625da5e1add7687086f4679169cde4
Alba López Aguilar <111623562+abetha64@users.noreply.github.com>
2024-04-15 12:49:18 -0600

    Update NLTK_file1.py

--- NLTK_file1.py
+++ NLTK_file1.py
@@ -2,15 +2,19 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://classroom.google.com/c/NjYxMDgwMjE1OTU4/a/NjI4NzMyMzAwNzY4/details"
+url = "https://latam.casadellibro.com/libros-ebooks-gratis/184"
 
 respuesta = requests.get(url)
 contenido_html = respuesta.text
 
 # Crear un objeto BeautifulSoup para analizar el HTML
 soup = BeautifulSoup(contenido_html, 'html.parser')
-# Extraer el texto visible de la página web
+
 texto_visible = soup.get_text()
+print("Texto visible en la página web:")
+print(texto_visible)
+
+print ("================================")
 
 # Expresión regular para encontrar palabras de 3 o 4 letras entre espacios
 expresion_regular = re.compile(r'\b\w{3,4}\b')
@@ -19,4 +23,3 @@ resultados_busqueda = expresion_regular.finditer(texto_visible)
 
 for resultado in resultados_busqueda:
     print(resultado.group(0))
-
